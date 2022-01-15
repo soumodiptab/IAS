58cb5870419ea7bed5d33697afa94271baf07a9d
Soumodipta Bose <sbose2019@gmail.com>
2022-01-15 13:19:45 +0530

    compaction of cmdbrowser

--- assignments/lab2/cmd-browser.py
+++ assignments/lab2/cmd-browser.py
@@ -1,45 +1,41 @@
 import socket
 import ssl
+from urllib.parse import urlparse
 # Configuration -----------------------------
-TARGET_PORT = 80
+HTTP_PORT = 80
 SSL_PORT = 443
 BUFFER_SIZE = 4096
 FORMAT = 'utf-8'
 # --------------------------------------------
 
 
-def secure_handler(url):
-    """secure_handler Handles ssl based http connection requests
+def url_validator(url):
+    p = urlparse(url)
+    if not (p.hostname and p.scheme):
+        return False
+    return True
 
-    Args:
-        url (string): Host/Domain of the 
 
-    Returns:
-        string: Returns the html page
-    """
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    secure_socket = ssl.wrap_socket(client, keyfile=None, certfile=None,
-                                    server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
-    secure_socket.connect((url, SSL_PORT))
-    request = f"GET / HTTP/1.1\r\nHost: {url}\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8\
-        \r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0\
-        \r\nConnection: Close\r\n\r\n"
-    secure_socket.send(request.encode())
-    response = response_handler(secure_socket)
-    status_code = response.split('\r\n')[0].split(' ')[1]
-    list_resp = response.split('\r\n\r\n')
-    list_resp.pop(0)
-    html_page = ''.join(list_resp)
-    secure_socket.close()
-    return html_page
+def ssl_wrapper(conn):
+    secure_socket = ssl.wrap_socket(conn, keyfile=None, certfile=None, server_side=False,
+                                    cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+    return secure_socket, SSL_PORT
+
+
+def request_handler(conn, parsedurl):
+    path = "/"
+    if parsedurl.path:
+        path = parsedurl.path
+    request = f"GET {path} HTTP/1.1\r\nHost: {parsedurl.hostname}\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8\
+            \r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0\
+            \r\nConnection: Close\r\n\r\n"
+    conn.send(request.encode())
 
 
 def response_handler(conn):
     """response_handler Handles Http responses in buffered mode and returns the response
-
     Args:
         conn (socket): 
-
     Returns:
         string: The response after decoding
     """
@@ -57,25 +53,30 @@ def start_browser():
     """start_browser Starts the command line browser
     """
     while True:
+        TARGET_PORT = HTTP_PORT
         print("------------------------------------------------------------------------------------------")
-        print(' CMD BROWSER | Enter URL')
+        print(' CMD BROWSER | Enter URL | EX: https://iiit.ac.in')
         print("------------------------------------------------------------------------------------------")
-        url = input()
-        TARGET_HOST = str(url)
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((TARGET_HOST, TARGET_PORT))
-        request = f"GET / HTTP/1.1\r\nHost: {TARGET_HOST}\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8\
-            \r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0\
-            \r\nConnection: Close\r\n\r\n"
-        client.send(request.encode())
-        response = response_handler(client)
+        url = str(input())
+        if url == 'exit':
+            break
+        if not url_validator(url):
+            print('Incorrect url correct format: https://www.google.com')
+            continue
+        parsed_url = urlparse(url)
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if parsed_url.scheme == "https":
+            conn, TARGET_PORT = ssl_wrapper(conn)
+        conn.connect((parsed_url.hostname, TARGET_PORT))
+        request_handler(conn, parsed_url)
+        response = response_handler(conn)
         status_code = response.split('\r\n')[0].split(' ')[1]
         list_resp = response.split('\r\n\r\n')
         list_resp.pop(0)
-        client.close()
+        conn.close()
         html_page = ''.join(list_resp)
-        if not status_code == '200':
-            html_page = secure_handler(TARGET_HOST)
+        # if not status_code == '200':
+        #     html_page = secure_handler(TARGET_HOST)
         print(html_page)
 
 
---
+++ resources/Readme.md
@@ -0,0 +1,4 @@
+# Things to Learn:
+* Socket Programming in Python
+* Implementing Http through sockets without library
+* Understanding RPC and implementing rpc over two different systems.
